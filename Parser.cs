2ec79f0b64261c57914fea93451e753135e65d01
praveshpansari <praveshpansari@gmail.com>
2020-11-27 22:45:56 +0545

    Added Parser
    Implemented a parser class which uses the tokens from the lexer and executes accordingly

---
+++ Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLEnvironment
+{
+    class Parser
+    {
+        Command command;
+        string[] parameters;
+
+        void Execute(Command command, String[] parameters)
+        {
+
+        }
+
+
+    }
+}
